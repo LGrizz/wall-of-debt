774965bc67a790fc72530148ceb348464ffd0f92
Kyle Langille <langille.kyle@gmail.com>
2014-02-17 22:01:56 -0700

    join button click event

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+$( document ).ready(function() {
+  
+	$( ".join" ).click(function() {
+  		$(".topaction").fadeOut(500, 0, function(){
+  			$(".joinform").fadeIn();
+  		});
+	});
 
+	$( ".joinbutton" ).click(function() {
+  		$(".topaction").fadeOut(500, 0, function(){
+  			$(".joinform").fadeIn();
+  		});
+	});
+
+
+});
