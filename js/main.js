144cb1ce97b2235873fb7150ef7b241b804bf369
Kyle Langille <langille.kyle@gmail.com>
2014-02-17 22:45:43 -0700

    scroll top animation on sticky header click

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ $( document ).ready(function() {
   		$(".topaction").fadeOut(500, 0, function(){
   			$(".joinform").fadeIn();
   		});
+
+  		$("html, body").animate({ scrollTop: 0 }, "slow");
 	});
 
 	$( ".joinbutton" ).click(function() {
