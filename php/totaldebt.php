3cd1c2661a7ff30a90203a3ada8c3b6d93626df9
Kyle Langille <langille.kyle@gmail.com>
2014-02-20 16:09:08 -0700

    sum total debt php

---
+++ php/totaldebt.php
@@ -0,0 +1,23 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$con=mysqli_connect("localhost","root","root","wallofdebt");
+// Check connection
+if (mysqli_connect_errno()){
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con, "SELECT sum(debt) from wallpost");  
+
+if (!$result) { // add this check.
+    die('Invalid query: ' . mysqli_error());
+}
+
+$row = mysqli_fetch_array($result);
+
+echo $row[0];
+
+mysqli_close($con);
+?>
