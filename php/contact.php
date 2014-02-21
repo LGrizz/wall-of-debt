d898a305fd2f3dabf49e7fdb053d54a0e3b90fff
Kyle Langille <langille.kyle@gmail.com>
2014-02-21 15:58:11 -0700

    php contact form

---
+++ php/contact.php
@@ -0,0 +1,18 @@
+<?php
+//Mail sending function
+            $subject = $_POST['name'];
+            $to = $_POST['email'];
+            $from = "zenphoto@example.com";
+
+            //data
+            $msg = "Your MSG <br>\n";       
+
+
+            //Headers
+            $headers  = "MIME-Version: 1.0\r\n";
+            $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+            $headers .= "From: <".$from. ">" ;
+
+            mail($to,$subject,$msg,$headers);
+            echo "Mail Sent.";
+ ?>
