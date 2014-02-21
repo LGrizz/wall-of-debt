d18333553abaff7819189ff53e7cd09895c31c17
levindixon <levindixon@gmail.com>
2014-02-21 03:01:48 -0700

    Cleaning up, minifying, beautifying...

--- js/home.js
+++ js/home.js
@@ -1,135 +1,135 @@
-$(document).ready(function(){
-                
+$(document).ready(function () {
+
     var page = 0;
     var distance = $('.welcome').offset().top;
-     $window = $(window);
-     var cardsArray = [];
-     var currentPage = 0;
-     var tweetsArray = [];
-     var gotTweets = false;
-     var wait = false;
-    
-        // Shuffle array
-        function Shuffle(o) {
-            for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-            return o;
-        };
+    $window = $(window);
+    var cardsArray = [];
+    var currentPage = 0;
+    var tweetsArray = [];
+    var gotTweets = false;
+    var wait = false;
 
-        // Get total debt and display
-        $.getJSON('php/totaldebt.php', function(data) {
-            $('.total').text('$'+data);
-            $('.total').fadeIn();
+    // Shuffle array
+    function Shuffle(o) {
+        for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    }
+
+    // Get total debt and display
+    $.getJSON('php/totaldebt.php', function (data) {
+        $('.total').text('$' + data);
+        $('.total').fadeIn();
+    });
+
+    // Get tweets put them in an array
+    $.getJSON('php/tweets.php', function (data) {
+        $.each(data.statuses, function (i, item) {
+            var obj = {};
+            obj.username = item.user.screen_name;
+            obj.tweet = item.text;
+            tweetsArray.push(obj);
+            if (tweetsArray.length === data.statuses.length) {
+                Shuffle(tweetsArray);
+                populateTweets();
+                gotTweets = true;
+            }
         });
+    });
 
-         // Get tweets put them in an array
-         $.getJSON('php/tweets.php', function(data) {
-                $.each( data.statuses, function( i, item ) {
-                     var obj = {};
-                    obj.username = item.user.screen_name;
-                    obj.tweet = item.text;
-                    tweetsArray.push(obj);
-                    if(tweetsArray.length === data.statuses.length) {
-                        Shuffle(tweetsArray)
-                        populateTweets();
-                        gotTweets = true;
-                    }
-                });
+    // Populate tweet boxes
+    var populateTweets = function () {
+        $('.tweet').each(function () {
+            $(this).find('img').fadeOut(function () {
+                $(this).parent().prepend("<p><span class='bold'>@" + tweetsArray[tweetsArray.length - 1].username + ":</span> " + tweetsArray[tweetsArray.length - 1].tweet + "</p>");
+                $(this).remove();
+                tweetsArray.pop();
             });
-
-        // Populate tweet boxes
-        var populateTweets = function(){
-            $('.tweet').each(function(){
-                $(this).find('img').fadeOut(function(){
-                    $(this).parent().prepend("<p><span class='bold'>@"+tweetsArray[tweetsArray.length-1].username+":</span> "+tweetsArray[tweetsArray.length-1].tweet+"</p>")
-                    $(this).remove();
-                    tweetsArray.pop();
-                })
-            })
-        };
+        });
+    };
 
 
     // Get first page from database
-    $.getJSON('php/wallposts.php?page='+page, function(data) {
-        $.each( data, function( i, item ) {
-                var obj = {};
-                obj.name = item.name;
-                obj.school = item.school;
-                obj.debt = item.debt;
-                cardsArray.push(obj);
-                if(cardsArray.length === data.length) {
-                        Shuffle(cardsArray)
-                        page++;
-                        showCards();
-                }
-         });
-    })
+    $.getJSON('php/wallposts.php?page=' + page, function (data) {
+        $.each(data, function (i, item) {
+            var obj = {};
+            obj.name = item.name;
+            obj.school = item.school;
+            obj.debt = item.debt;
+            cardsArray.push(obj);
+            if (cardsArray.length === data.length) {
+                Shuffle(cardsArray);
+                page++;
+                showCards();
+            }
+        });
+    });
 
     // Show 5 cards, tweet displayed and placed randomly in set
-    showCards = function(){
-        var randomNum = Math.floor(Math.random() * (currentPage - (currentPage+5) + 1)) + (currentPage+5);
+    showCards = function () {
+        var randomNum = Math.floor(Math.random() * (currentPage - (currentPage + 5) + 1)) + (currentPage + 5);
         for (var i = 0; i < 5; i++) {
-            if(cardsArray.length !== 0){
-                if(cardsArray[cardsArray.length-1].debt.length > 5) {
-                    $('.wall-cards').append("<div class='col-md-3 col-sm-4 item animate'><div class='card'><p class='name'>"+cardsArray[cardsArray.length-1].name+"</p><p class='debt large'>$"+cardsArray[cardsArray.length-1].debt+"</p><p class='school'>"+cardsArray[cardsArray.length-1].school+"</p></div></div>");
+            if (cardsArray.length !== 0) {
+                if (cardsArray[cardsArray.length - 1].debt.length > 5) {
+                    $('.wall-cards').append("<div class='col-md-3 col-sm-4 item animate'><div class='card'><p class='name'>" + cardsArray[cardsArray.length - 1].name + "</p><p class='debt large'>$" + cardsArray[cardsArray.length - 1].debt + "</p><p class='school'>" + cardsArray[cardsArray.length - 1].school + "</p></div></div>");
                     cardsArray.pop();
-                }else{
-                    $('.wall-cards').append("<div class='col-md-3 col-sm-4 item animate'><div class='card'><p class='name'>"+cardsArray[cardsArray.length-1].name+"</p><p class='debt'>$"+cardsArray[cardsArray.length-1].debt+"</p><p class='school'>"+cardsArray[cardsArray.length-1].school+"</p></div></div>");
+                } else {
+                    $('.wall-cards').append("<div class='col-md-3 col-sm-4 item animate'><div class='card'><p class='name'>" + cardsArray[cardsArray.length - 1].name + "</p><p class='debt'>$" + cardsArray[cardsArray.length - 1].debt + "</p><p class='school'>" + cardsArray[cardsArray.length - 1].school + "</p></div></div>");
                     cardsArray.pop();
                 }
 
-                if(i == randomNum-1 && i < cardsArray.length) {
-                    if(gotTweets){
-                        $('.wall-cards').append("<div class='col-md-3 col-sm-4 item animate'><div class='card tweet'><p><span class='bold'>@"+tweetsArray[tweetsArray.length-1].username+":</span> "+tweetsArray[tweetsArray.length-1].tweet+"</p><div class='bird'></div></div></div>");
+                if (i == randomNum - 1 && i < cardsArray.length) {
+                    if (gotTweets) {
+                        $('.wall-cards').append("<div class='col-md-3 col-sm-4 item animate'><div class='card tweet'><p><span class='bold'>@" + tweetsArray[tweetsArray.length - 1].username + ":</span> " + tweetsArray[tweetsArray.length - 1].tweet + "</p><div class='bird'></div></div></div>");
                         tweetsArray.pop();
-                    }else{
+                    } else {
                         $('.wall-cards').append("<div class='col-md-3 col-sm-4 item animate'><div class='card tweet'><img src='img/loading.gif' class='img-responsive logo' alt='Tweet Loading'><div class='bird'></div></div></div>");
                     }
+                }
             }
-            }
-        };
+        }
 
-    }
+    };
 
     // When window is scrolled....
 
-    $window.scroll(function() {
+    $window.scroll(function () {
 
         // Infinite Scroll
 
         if ($(window).scrollTop() >= $(document).height() - $(window).height() - 250) {
 
-           if(cardsArray.length === 0 && !wait){
+            if (cardsArray.length === 0 && !wait) {
                 wait = true;
-                $.getJSON('php/wallposts.php?page='+page, function(data) {
-                    $.each( data, function( i, item ) {
+                $.getJSON('php/wallposts.php?page=' + page, function (data) {
+                    $.each(data, function (i, item) {
                         var obj = {};
                         obj.name = item.name;
                         obj.school = item.school;
                         obj.debt = item.debt;
                         cardsArray.push(obj);
-                        if(cardsArray.length === data.length) {
-                                Shuffle(cardsArray)
-                                page++;
-                                wait = false;
-                                showCards();
+                        if (cardsArray.length === data.length) {
+                            Shuffle(cardsArray);
+                            page++;
+                            wait = false;
+                            showCards();
                         }
-                 });
-                })
-           }else{
-            showCards();
-           }
-           
+                    });
+                });
+            } else {
+                showCards();
+            }
+
         }
 
         // Fixed Nav + To Top Button
 
 
-        if ( $window.scrollTop() >= distance ) {
+        if ($window.scrollTop() >= distance) {
             $('.navbar').show();
             $('.to-top').fadeIn();
-        }else if( $window.scrollTop() <= distance ) {
-             $('.navbar').hide();
-             $('.to-top').hide();
+        } else if ($window.scrollTop() <= distance) {
+            $('.navbar').hide();
+            $('.to-top').hide();
         }
 
 
--- js/main.js
+++ js/main.js
@@ -1,85 +1,92 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-	String.prototype.capitalize = function(lower) {
-	    return (lower ? this.toLowerCase() : this).replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
-	};
+    String.prototype.capitalize = function (lower) {
+        return (lower ? this.toLowerCase() : this).replace(/(?:^|\s)\S/g, function (a) {
+            return a.toUpperCase();
+        });
+    };
 
-	function IsEmail(email) {
-	  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	  return regex.test(email);
-	}
-  
-	$('#submitBrick').click(function(){
-		
-		var capName = $('#name').val().capitalize(true);
-		$("#name").val(capName)
-		$("#debt").val($("#debt").val().replace(/[^\d.]/g, ""));
+    function IsEmail(email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    }
 
-		if( IsEmail($('#email').val()) && $("#name").val() != '' && $("#debt").val() != '' && $("#school").val() != ''){
-			$.post( "php/form.php", $( "#join" ).serialize(), function( data ) {
-				$('#join-form form').fadeOut(function(){
-					$('#join-form form').empty();
-					$('.thank-you-message').fadeIn()
-				});
-			});
-		} else{
-			if(!IsEmail($('#email').val())){
-				$('#email').css("border-bottom-color", "#e8432e")
-			}else{
-				$('#email').css("border-bottom-color", "#fff")
-			}
-			if($("#name").val() == '') {
-				$('#name').css("border-bottom-color", "#e8432e")
-			}else{
-				$('#name').css("border-bottom-color", "#fff")
-			}
-			if($("#school").val() == '') {
-				$('#school').css("border-bottom-color", "#e8432e")
-			}else{
-				$('#school').css("border-bottom-color", "#fff")
-			}
-			if($("#debt").val() == '') {
-				$('#debt').css("border-bottom-color", "#e8432e")
-			}else{
-				$('#debt').css("border-bottom-color", "#fff")
-			}
-		}
+    $('#submitBrick').click(function () {
 
-		
-	})
+        var capName = $('#name').val().capitalize(true);
+        $("#name").val(capName);
+        $("#debt").val($("#debt").val().replace(/[^\d.]/g, ""));
 
-
-
-	var schools = [];
-
-	$.getJSON('json/schools.json', function(data) {
-            	schools = data.schools;
-            }).done(function(){
-            	$('#school').autocomplete({
-		    lookup: schools
-		});
+        if (IsEmail($('#email').val()) && $("#name").val() !== '' && $("#debt").val() !== '' && $("#school").val() !== '') {
+            $.post("php/form.php", $("#join").serialize(), function (data) {
+                $('#join-form form').fadeOut(function () {
+                    $('#join-form form').empty();
+                    $('.thank-you-message').fadeIn();
+                });
             });
+        } else {
+            if (!IsEmail($('#email').val())) {
+                $('#email').css("border-bottom-color", "#e8432e");
+            } else {
+                $('#email').css("border-bottom-color", "#fff");
+            }
+            if ($("#name").val() === '') {
+                $('#name').css("border-bottom-color", "#e8432e");
+            } else {
+                $('#name').css("border-bottom-color", "#fff");
+            }
+            if ($("#school").val() === '') {
+                $('#school').css("border-bottom-color", "#e8432e");
+            } else {
+                $('#school').css("border-bottom-color", "#fff");
+            }
+            if ($("#debt").val() === '') {
+                $('#debt').css("border-bottom-color", "#e8432e");
+            } else {
+                $('#debt').css("border-bottom-color", "#fff");
+            }
+        }
 
-	
+
+    });
 
 
 
-	$('.joinbutton').click(function(){
-		$("html, body").animate({ scrollTop: 0 }, "slow", function(){
-			$('#main').fadeOut(500, function(){
-				$('#join-form').fadeIn(500);
-			})		
-		});
-		
-	})
+    var schools = [];
 
-	$(".contactus").click(function(){
-		$("html, body").animate({ scrollTop: $('.welcome.contact-us').offset().top }, 400);
-	});
+    $.getJSON('json/schools.json', function (data) {
+        schools = data.schools;
+    }).done(function () {
+        $('#school').autocomplete({
+            lookup: schools
+        });
+    });
 
-	$(".to-top").click(function(){
-		$("html, body").animate({ scrollTop: 0 }, "slow");
-	})
+
+
+
+    $('.joinbutton').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, "slow", function () {
+            $('#main').fadeOut(500, function () {
+                $('#join-form').fadeIn(500);
+            });
+        });
+
+    });
+
+    $(".contactus").click(function () {
+        $("html, body").animate({
+            scrollTop: $('.welcome.contact-us').offset().top
+        }, 400);
+    });
+
+    $(".to-top").click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, "slow");
+    });
 
 
 });
